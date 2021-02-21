f39b35e25958e9cc9bf2574b39215d5d8670e74e
César Cervantes <ces_29_1995@hotmail.com>
2021-02-21 01:17:45 -0600

    Finish styles for the inicio view

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Carousel.css';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
+
 function Carousel() {
+    let mediaMatch = window.matchMedia('(min-width: 750px)');
+    const [matches, setMatches] = useState(mediaMatch.matches);
+    useEffect(()=>{
+        const handler = event=>setMatches(event.matches);
+        mediaMatch.addEventListener('change', handler);
+        return ()=> mediaMatch.removeEventListener('change', handler);
+    });
+    let styles = {
+        position: 'relative',
+        width: matches ? '50%' : '90%',
+        height: matches ? '90vh' : '80vh'
+    };
     return (
             <AwesomeSlider
                 media={[
@@ -17,11 +30,7 @@ function Carousel() {
                     source: './images/leche.jpeg',
                 },
                 ]}
-                style={{
-                    position: 'relative',
-                    width: '90%',
-                    height: '80vh'
-                }}
+                style={styles}
                 mobileTouch={true}
             />
     );
