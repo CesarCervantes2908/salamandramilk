a1b5bdc7c379a2e57605374cf672d88dc7894c41
César Cervantes <ces_29_1995@hotmail.com>
2021-02-21 00:01:53 -0600

    Finish mobile styles

--- src/components/ExtraInfo.js
+++ src/components/ExtraInfo.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import '../styles/ExtraInfo.css';
 
 function ExtraInfo() {
     return (
         <div className="extra-info">
-            <p>
+            <p id="first" className="extrainfo-paragraph">
                 Somos una empresa dedicada a birndar la mejor alternativa de alimentación a tu vida, saludable y nutritia.
 Únete a nosotros y sé parte de la familia SlamandraMilk. Disfruta del placer y  los beneficios de una alimentación que está al alcance de tus manos.
             </p>
-            <p>
+            <p id="second" className="extrainfo-paragraph">
                 Aprovecha todos los beneficios del Kéfir en diferentes productos. Ideal para los intolerantes a la lactosa, en el proceso de fermentación se logra eliminarla casi en su totalidad.
 Prueba las maravillas nutricionales de los frutos secos que nos ofrece la naturaleza.
             </p>
-            <p>
+            <p id="third" className="extrainfo-paragraph">
                 Alguna contraindicación del Kéfir podría ser para las personas con problemas de intestino permeable. No por esto, deja de ser un alimento por excelencia nutritivo que contiene probióticos vivos de varias cepas; proteinas y calcio; vitamina D y B; magnesio, potasio y minerales. 
             </p>
         </div>
