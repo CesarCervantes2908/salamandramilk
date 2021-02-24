29883d83adb7245e968bf14744bd30cb4c14f2a3
César Cervantes <ces_29_1995@hotmail.com>
2021-02-23 19:17:09 -0600

    Create Contacto components content. Styles missing

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Separador from './Separador';
 
 function Contacto() {
     return (
-        <main>
+        <>
             <NavBar></NavBar>
-            Contacto
-        </main>
+            <main>
+                <img className="logo" src="./images/Logo.png" />
+                <Separador>¡Realiza tu pedido!</Separador>
+                <button>
+                    <img src="" alt="" id="whatssapp-logo"/>
+                    Envianos un Mensaje
+                </button>
+                <h1 className="title">SalamandraMilk</h1>
+                <p className="ubication">Gustavo A. Madero, Mexico City, Mexico</p>
+                <p className="email">salamandramilk@gmail.com</p>
+            </main>
+        </>
     );
 };
 
