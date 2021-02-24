ab9224740f2095f465c7aa98dc572cef803e78d1
César Cervantes <ces_29_1995@hotmail.com>
2021-02-23 19:10:34 -0600

    Create content of Menu component. Styles missing

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,69 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Separador from './Separador';
+import { leches } from '../utils/Productos';
+import { kefir } from '../utils/Productos';
+import ProductCard from './ProductCard';
 
 function Menu() {
+    const renderLeches = () => {
+        return leches.map(leche => {
+            return (
+                <ProductCard title={leche.title}
+                             className="leche-card"
+                             imgageURL={leche.imgageURL}
+                             price={leche.price}
+                             key={leche.title}>
+                                {leche.description}
+                </ProductCard>
+                );
+        });
+    };
+    const renderKefir = () => {
+        return kefir.map(product => {
+            return (
+                <ProductCard title={product.title}
+                             className="kefir-card"
+                             imgageURL={product.imgageURL}
+                             price={product.price}
+                             key={product.title}>
+                                {product.description}
+                </ProductCard>
+                );
+        });
+    };
     return (
-        <main>
-        <NavBar></NavBar>
-            Menú
-        </main>
+        <>
+            <NavBar></NavBar>
+            <main>
+                <h1 className="title">Menú</h1>
+                <img className="logo" src="./images/Logo.png" />
+                <Separador> Lista de Precios</Separador>
+                <section id="Leches">
+                    <h2 className="section-title" >
+                        Leche Vegana
+                    </h2>
+                    <p className="section-description">
+                        Entre otros beneficios las leches vegetales reducen el colesterol son bajas en calorías y grasas, e incluso previenen ciertos tipos de cáncer. No contiene conservadores ni endulzantes. Dura de 4 a 5 días en refrigeración a partir de su elaboración. Venta por litro.
+                    </p>
+                    <div className="product-container leches">
+                        {renderLeches()}
+                    </div>
+                </section>
+                <Separador></Separador>
+                <section id="Kefir">
+                    <h2 className="section-title" >
+                        Kéfir
+                    </h2>
+                    <p className="section-description">
+                        No contiene endulzantes ni conservadores. Dura hasta 15 días en refrigeración a partir de la fecha de elaboración.
+                    </p>
+                    <div className="product-container leches">
+                        {renderKefir()}
+                    </div>
+                </section>
+            </main>
+        </>
     );
 };
 
---
+++ src/utils/Productos.js
@@ -0,0 +1,41 @@
+let leches = [
+    {
+    title: 'Bebida de Almendras',
+    price: 50,
+    imgageURL: '',
+    description: 'Por su alto contenido de fosforo y magnesio, las almendras nos ayudan a tener unos huesos fuertes, cuidan nuestros músculos, combaten la ansiedad y favorecen un buen descan; además, por su contenido de calcio previene la osteoporosis.'
+}, {
+    title: 'Bebida de Nuez',
+    price: 50,
+    imgageURL: '',
+    description: 'Reduce el colesterol por su alto contenido en ácidos gracos omega3 de origen vegetal, ayuda a prevenir enfermedades cardiovasculares y tener un corazón sano, cuida tu cerebro y regula el tránsito intestinal, entre muchos otros beneficios que nos ofrece.'
+}, {
+    title: 'Bebida de Avena',
+    price: 40,
+    imgageURL: '',
+    description: 'Ayuda a reducir el colesterol malo, aumenta tus defensas, ayuda a perder peso y mejora tu digestión, solo por mencionar algunos beneficios de este maravilloso cereal.'
+}, {
+    title: 'Bebida de Arroz',
+    price: 40,
+    imgageURL: '',
+    description: 'Es una potente fuente de energía con alto contenido de fibra, bajo en grasa y alto contenido en hierro.'
+}];
+let kefir = [{
+    title: 'Leche de Kéfir',
+    price: 70,
+    imgageURL: '',
+    description: 'Venta por litro.'
+}, {
+    title: 'Yogur Griego',
+    price: 35,
+    imgageURL: '',
+    description: '220g. Libre de conservadores y endul<antes artificiales. A elegir: \nAlmendra + miel \nAvena + miel \nAmaranto + miel \nNuez + miel'
+}, {
+        title: 'Hot Cakes',
+        price: 70,
+        imgageURL: '',
+        description: '12 piezas. \nLibre de gluten. conservadores y azúcares. Elaborado con leche de Kéfir y mantequilla de coco. Ideal para acompañar con dulce o salado.'
+    }];
+
+export {leches};
+export {kefir};
