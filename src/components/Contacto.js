355d05de76991273952b219e193da3ca261220f9
César Cervantes <ces_29_1995@hotmail.com>
2021-02-25 15:21:28 -0600

    Finish all mobile styles

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Separador from './Separador';
+import '../styles/Contacto.css';
 
 function Contacto() {
+    const handleClick = (event)=>{
+        window.open('https://wa.me/+525516265721', '_blank')
+    }
     return (
         <>
             <NavBar></NavBar>
             <main>
                 <img className="logo" src="./images/Logo.png" />
                 <Separador>¡Realiza tu pedido!</Separador>
-                <button>
-                    <img src="" alt="" id="whatssapp-logo"/>
-                    Envianos un Mensaje
+                <button id="wa-button" onClick={handleClick}>
+                    Envianos un Mensaje 
+                    <img src="./images/wa-logo.png" alt="" id="whatssapp-logo"/>
                 </button>
                 <h1 className="title">SalamandraMilk</h1>
                 <p className="ubication">Gustavo A. Madero, Mexico City, Mexico</p>
