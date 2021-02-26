e61637086ed1515361a4c276fad2567af537ebdc
César Cervantes <ces_29_1995@hotmail.com>
2021-02-26 14:41:54 -0600

    Finish styles. Add missing images for products

--- src/utils/Productos.js
+++ src/utils/Productos.js
@@ -3,25 +3,25 @@ let leches = [
     title: 'Bebida de Almendras',
     price: 50,
     quantity: '1L',
-    imgageURL: './images/almendras.jpeg',
+    imgageURL: './images/almendra.jpeg',
     description: 'Por su alto contenido de fosforo y magnesio, las almendras nos ayudan a tener unos huesos fuertes, cuidan nuestros músculos, combaten la ansiedad y favorecen un buen descan; además, por su contenido de calcio previene la osteoporosis.'
 }, {
     title: 'Bebida de Nuez',
     price: 50,
     quantity: '1L',
-    imgageURL: './images/almendras.jpeg',
+    imgageURL: './images/nuez.jpeg',
     description: 'Reduce el colesterol por su alto contenido en ácidos gracos omega3 de origen vegetal, ayuda a prevenir enfermedades cardiovasculares y tener un corazón sano, cuida tu cerebro y regula el tránsito intestinal, entre muchos otros beneficios que nos ofrece.'
 }, {
     title: 'Bebida de Avena',
     price: 40,
     quantity: '1L',
-    imgageURL: './images/almendras.jpeg',
+    imgageURL: './images/avena.jpeg',
     description: 'Ayuda a reducir el colesterol malo, aumenta tus defensas, ayuda a perder peso y mejora tu digestión, solo por mencionar algunos beneficios de este maravilloso cereal.'
 }, {
     title: 'Bebida de Arroz',
     price: 40,
     quantity: '1L',
-    imgageURL: './images/almendras.jpeg',
+    imgageURL: './images/arroz.jpeg',
     description: 'Es una potente fuente de energía con alto contenido de fibra, bajo en grasa y alto contenido en hierro.'
 }];
 let kefir = [{
